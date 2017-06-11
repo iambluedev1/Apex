42051a383e404459d39ac9a8f4d6ee987d7c3a48
Iambluedev <iambluedev@gmx.fr>
2017-06-11 20:12:58 +0200

    az

---
+++ Apex-master/src/main/java/fr/iambluedev/vulkan/backend/DefaultMcBackend.java
@@ -0,0 +1,11 @@
+package fr.iambluedev.vulkan.backend;
+
+import de.jackwhite20.apex.util.BackendInfo;
+
+public class DefaultMcBackend extends BackendInfo{
+
+	public DefaultMcBackend() {
+		super("defaultMcBackend", "164.132.48.233", 25577);
+	}
+
+}
