e9506b2aa36183c1420eceabea6e2fda55960e56
Iambluedev <iambluedev@gmx.fr>
2017-06-19 13:27:02 +0200

    test

--- Apex-master/src/main/java/fr/iambluedev/vulkan/command/CloseCommand.java
+++ Apex-master/src/main/java/fr/iambluedev/vulkan/command/CloseCommand.java
@@ -2,8 +2,11 @@ package fr.iambluedev.vulkan.command;
 
 import de.jackwhite20.apex.Apex;
 import de.jackwhite20.apex.command.Command;
+import fr.iambluedev.spartan.utils.Callback;
+import fr.iambluedev.spartan.utils.RedisJsonMessage;
 import fr.iambluedev.vulkan.Vulkan;
 import fr.iambluedev.vulkan.state.ListeningState;
+import redis.clients.jedis.Jedis;
 
 public class CloseCommand extends Command{
 	
@@ -15,6 +18,18 @@ public class CloseCommand extends Command{
 	public boolean execute(String[] args) {
 		Vulkan.getInstance().setListeningState(ListeningState.CLOSE);
 		Apex.getLogger().info("The default port is now closed !");
+		
+		new Thread(new Runnable(){
+			@Override
+			public void run() {
+				Vulkan.getInstance().getRedis().get(new Callback<Jedis>() {
+					@Override
+					public void call(Jedis jedis) {
+						jedis.publish("node", new RedisJsonMessage().setCmd("hasEnoughtForGm").setContent("hg111k").get());
+					}
+				});
+			}
+		}).start();
 		return true;
 	}
 
